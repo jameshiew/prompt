2f0194020ee413500c6f1a09728bebb95fd8cf3d
James Hiew <james@hiew.net>
2024-12-15 12:07:34 +0000

    WIP parsing git revspecs

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,8 @@ use tracing_subscriber::EnvFilter;
 #[command(version)]
 struct Cli {
     path: Option<PathBuf>,
+    #[arg(short, long)]
+    git: Option<String>,
 }
 
 #[tokio::main]
@@ -21,6 +23,41 @@ async fn main() -> Result<()> {
         .expect("should be able to initialize the logger");
     let cli = Cli::parse();
     let path = cli.path.unwrap_or_else(|| PathBuf::from("."));
+
+    if let Some(revspec) = cli.git {
+        let repo = gix::open(".")?;
+        let object = repo
+            .rev_parse_single(revspec.as_str())?
+            .object()?
+            .peel_tags_to_end()?; // TODO: HEAD..HEAD~1
+        let data = match object.kind {
+            gix::objs::Kind::Tree => &object.into_tree().data.clone(),
+            gix::objs::Kind::Blob => &object.into_blob().data.clone(),
+            gix::objs::Kind::Commit => {
+                let commit = &object.into_commit();
+                let tree = commit.tree()?;
+                let mut data = vec![];
+                // TODO: goes on infinitely
+                while let Ok(tree_ref) = tree.decode() {
+                    for entry in &tree_ref.entries {
+                        let obj = repo.find_object(entry.oid)?;
+                        let mut file_contents = match obj.kind {
+                            gix::objs::Kind::Tree => obj.into_tree().data.clone(),
+                            gix::objs::Kind::Blob => obj.into_blob().data.clone(),
+                            gix::objs::Kind::Commit => todo!(),
+                            gix::objs::Kind::Tag => todo!(),
+                        };
+                        data.append(&mut file_contents);
+                    }
+                }
+                &data.clone()
+            }
+            gix::objs::Kind::Tag => &object.into_tag().data.clone(),
+        };
+        println!("{}", String::from_utf8_lossy(&data));
+        return Ok(());
+    }
+
     let all_files = DashMap::new();
 
     WalkBuilder::new(&path)
