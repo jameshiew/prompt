861948963dd6566de188e57963040f26a7200ccc
James Hiew <james@hiew.net>
2024-12-19 23:24:31 +0000

    Tokenize chunks at a time

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,7 +1,20 @@
+use std::ops::Deref;
+
+use text_splitter::{ChunkConfig, TextSplitter};
 use tiktoken_rs::o200k_base_singleton;
 
 pub fn tokenize(text: &str) -> Vec<u32> {
     let bpe = o200k_base_singleton();
     let bpe = bpe.lock();
-    bpe.encode_with_special_tokens(text)
+
+    // tokenize chunks of text at a time in case the text is large
+    // to avoid tokenizer stack overflow
+    let max_tokens = 1_000;
+    let splitter = TextSplitter::new(ChunkConfig::new(max_tokens).with_sizer(bpe.deref()));
+    let chunks = splitter.chunks(text);
+    let mut tokens = vec![];
+    for chunk in chunks {
+        tokens.append(&mut bpe.encode_with_special_tokens(chunk));
+    }
+    tokens
 }
