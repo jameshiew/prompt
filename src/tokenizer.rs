38a728f2cfda44fe1a5797961f79121f100737d7
James Hiew <james@hiew.net>
2024-12-22 00:24:46 +0000

    Speed up tokenizing

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,19 +1,19 @@
-use std::ops::Deref;
-
 use text_splitter::{ChunkConfig, TextSplitter};
 use tiktoken_rs::o200k_base_singleton;
 
-const CHUNK_CAPACITY: usize = 100_000;
+const CHUNK_CAPACITY: usize = 2_000_000;
 
 pub fn tokenize(text: &str) -> Vec<u32> {
     let bpe = o200k_base_singleton();
     let bpe = bpe.lock();
 
-    let splitter = TextSplitter::new(ChunkConfig::new(CHUNK_CAPACITY).with_sizer(bpe.deref()));
+    let splitter = TextSplitter::new(ChunkConfig::new(CHUNK_CAPACITY));
     let chunks = splitter.chunks(text);
-    let mut tokens = vec![];
-    for chunk in chunks {
-        tokens.append(&mut bpe.encode_with_special_tokens(chunk));
-    }
+
+    let tokens: Vec<_> = chunks
+        .into_iter()
+        .flat_map(|chunk| bpe.encode_with_special_tokens(chunk))
+        .collect();
+
     tokens
 }
